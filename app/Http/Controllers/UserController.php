9cccae1f06d863acc54f3fa37bf9cd6e2293cf9d
EsraaHamada100 <esraahamada654@gmail.com>
2023-06-19 13:17:11 +0300

    validation and details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request){
         $incomingField = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required',
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            // this confirmed text will make sure that the password_confirmation
+            // match the password it self but you should name the field anything_confirmation
+            'password' => ['required', 'min:8', 'confirmed'],
         ]);
+
+        // hashing the password
+        $incomingField['password'] = bcrypt($incomingField['password']);
+        
         // create a new record in the users table
         User::create($incomingField);
         return 'Hello from register function';
