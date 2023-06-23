d9a9cccb1d8f0f985705e11100db677a183dfa32
EsraaHamada100 <esraahamada654@gmail.com>
2023-06-23 05:27:21 +0300

    Middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,5 +1,5 @@
 <?php
-
+// THis is the guest Middleware
 namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect('/');
             }
         }
 
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,26 @@ use App\Http\Controllers\UserController;
 |
 */
 
-
+/**
+ * Middleware guest is a given middleware that allows only guest to enter the route
+ * Middleware auth is a given middleware that allows only authenticated users to enter the route
+ * 
+ */
 
 // User related routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
-Route::post('/register', [UserController::class, "register"]);
-Route::post('/login', [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, "logout"]);
+
+// we named it login because auth middleware will redirect to something named
+// login and we wanna it to redirect him to '/' so we name it login 
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class, "register"])->middleware('guest');
+Route::post('/login', [UserController::class, "login"])->middleware('guest');
+Route::post('/logout', [UserController::class, "logout"])->middleware('auth');
 
 // Blog post routes
-Route::get('create-post', [PostController::class, "showCreateForm"]);
-Route::post('create-post', [PostController::class, "storeNewPost"]);
+Route::get('create-post', [PostController::class, "showCreateForm"])->middleware('guest');
+Route::post('create-post', [PostController::class, "storeNewPost"])->middleware('auth');
 // here {post} means the id of post that will be passed 
-Route::get('post/{post}', [PostController::class, "viewSinglePost"]);
+Route::get('post/{post}', [PostController::class, "viewSinglePost"])->middleware('mustBeLoggedIn');
 
 
 
